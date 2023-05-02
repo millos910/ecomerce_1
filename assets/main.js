531a165ecc63686b325e493ec203ce6a6f988dc6
millos910 <emontero910@gmail.com>
2023-05-02 06:13:09 -0500

    mi primer e-comerce avance 1

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+
+const btnCart=document.querySelector('.contenedor-icon-principal')
+const conteinerCartProducts=document.querySelector('.hidden-cart')
+btnCart.addEventListener('click',()=>{conteinerCartProducts.classList.toggle('hidden-cart')})
