154f57804bab7b9462a4bfb579c770440cd941ef
millos910 <emontero910@gmail.com>
2023-05-08 15:15:11 -0500

    bug de boton modo nocturno

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,10 @@
 const btnCart=document.querySelector('.contenedor-icon-principal')
 const conteinerCartProducts=document.querySelector('.hidden-cart')
 btnCart.addEventListener('click',()=>{conteinerCartProducts.classList.toggle('hidden-cart')})
+
+
+const  btnSwitch=document.querySelector('.switch');
+btnSwitch.addEventListener('click',()=> {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+})
